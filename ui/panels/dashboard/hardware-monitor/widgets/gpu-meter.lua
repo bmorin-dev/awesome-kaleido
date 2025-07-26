fc4d0e3843f0e500f61245e2ab91e1f736360acf
bmorin-dev <robot0324@gmail.com>
2025-07-25 22:05:23 -0400

    fixed gpu monitor

--- ui/panels/dashboard/hardware-monitor/widgets/gpu-meter.lua
+++ ui/panels/dashboard/hardware-monitor/widgets/gpu-meter.lua
@@ -1,72 +1,78 @@
-local wibox = require('wibox')
-local gears = require('gears')
-local beautiful = require('beautiful')
-local watch = require('awful.widget.watch')
-local icons = require('theme.icons')
+local wibox = require("wibox")
+local gears = require("gears")
+local beautiful = require("beautiful")
+local awful = require("awful")
+local icons = require("theme.icons")
 
 local dpi = beautiful.xresources.apply_dpi
 
 local gpu = wibox.widget.textbox()
 gpu.font = beautiful.widget_text
 
-local slider = wibox.widget {
+local slider = wibox.widget({
 	nil,
 	{
-		id               = 'gpu_usage',
-		max_value        = 100,
-		value            = 0,
-		forced_height    = dpi(2),
-		color            = beautiful.fg_normal,
+		id = "gpu_usage",
+		max_value = 100,
+		value = 0,
+		forced_height = dpi(2),
+		color = beautiful.fg_normal,
 		background_color = beautiful.groups_bg,
-		shape            = gears.shape.rounded_rect,
-		widget           = wibox.widget.progressbar
+		shape = gears.shape.rounded_rect,
+		widget = wibox.widget.progressbar,
 	},
 	nil,
-	expand = 'none',
-	layout = wibox.layout.align.vertical
-}
+	expand = "none",
+	layout = wibox.layout.align.vertical,
+})
 
-gpu.text = "N/A"
---watch(
---	[[bash -c "free | grep -z Mem.*Swap.*"]], 2, function(_, stdout)
---		local total, used, free, shared, buff_cache, available, total_swap, used_swap, free_swap =
---			stdout:match('(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*Swap:%s*(%d+)%s*(%d+)%s*(%d+)')
---		slider.gpu_usage:set_value(used / total * 100)
---		gpu.text = math.floor(used * 10 / 1048576) / 10 .. "G"
---		collectgarbage('collect')
---	end
---)
+local max_freq = 1100
 
-local ram_meter = wibox.widget {
+local function update_max_freq()
+	awful.spawn.easy_async_with_shell([[cat /sys/class/drm/card1/gt_max_freq_mhz]], function(stdout)
+		max_freq = stdout:match("%d+") or 1100
+	end)
+end
+
+update_max_freq()
+
+awful.widget.watch([[cat /sys/class/drm/card1/gt_cur_freq_mhz]], 2, function(_, stdout)
+	local freq = stdout:match("%d+")
+	slider.gpu_usage:set_value(freq / max_freq * 100)
+	gpu.text = math.floor((freq / max_freq) * 100) .. "%"
+	collectgarbage("collect")
+end)
+
+local ram_meter = wibox.widget({
 	{
 		{
 			{
 				{
 					image = icons.applets.resources.gpu,
 					resize = true,
-					widget = wibox.widget.imagebox
+					widget = wibox.widget.imagebox,
 				},
 				top = dpi(12),
 				bottom = dpi(12),
-				widget = wibox.container.margin
+				widget = wibox.container.margin,
 			},
 			{
 				gpu,
 				fg = beautiful.fg_normal,
-				widget = wibox.container.background
+				widget = wibox.container.background,
 			},
-			spacing      = dpi(4),
-			layout       = wibox.layout.fixed.horizontal,
+			spacing = dpi(4),
+			layout = wibox.layout.fixed.horizontal,
 			forced_width = dpi(65),
 		},
 		slider,
 		spacing = dpi(12),
-		layout = wibox.layout.fixed.horizontal
+		layout = wibox.layout.fixed.horizontal,
 	},
 	left = dpi(24),
 	right = dpi(24),
 	forced_height = dpi(48),
-	widget = wibox.container.margin
-}
+	widget = wibox.container.margin,
+})
 
 return ram_meter
